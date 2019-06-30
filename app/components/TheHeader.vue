5a73d6d15b963e7e057d93ee7f1d5397a347d52f
tana <gogo71889@gmail.com>
2019-06-30 13:34:08 +0900

    init project

---
+++ app/components/TheHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="header-container">
+    <ul id="header">
+      <li><nuxt-link to="/">Pokémon Portfolio</nuxt-link></li>
+    </ul>
+  </div>
+</template>
+
+<style>
+#header-container {
+  padding: 20px 0;
+  background-color: #000000;
+}
+#header li {
+  display: inline-block;
+  margin: 0 20px;
+  font-weight: bold;
+}
+#header a {
+  color: #ffffff;
+  text-decoration: none;
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,57 @@
+
+export default {
+  mode: 'universal',
+  srcDir: 'app',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    '~/assets/common.css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/axios'
+  ],
+
+  axios: {
+    // 自分のfirebaseアカウントを設定
+    baseURL: 'http://localhost:8888'
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
